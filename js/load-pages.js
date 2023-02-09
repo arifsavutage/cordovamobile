8541114e7bbac65f6a7a50f905fcec38bd110844
arifsavutage <arifsavutage@gmail.com>
2023-02-09 09:34:01 +0700

    initial commit

---
+++ js/load-pages.js
@@ -0,0 +1,257 @@
+var list_tips = [
+  [
+    'https://images.unsplash.com/photo-1542990253-a781e04c0082?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
+    'seed',
+    '5 Cara Memilih Bibit Unggul Untuk Tanaman Pertamamu',
+    'Aliquam aliquet enim et eros viverra, in mattis ante semper.',
+    `<p align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed justo est. Cras dignissim, turpis quis efficitur
+                faucibus, erat enim porta mi, ut mollis massa arcu sed mauris. In in ligula ac purus pretium rutrum venenatis sed enim.
+                Vivamus lacus mi, laoreet vel dignissim non, faucibus sed neque. Mauris venenatis malesuada velit. Etiam auctor, leo et
+                euismod luctus, neque tellus tempor dui, et lacinia ex ante sed tortor. Nunc elementum mollis lacus, sed facilisis
+                sapien malesuada ut. Quisque rhoncus nunc non diam scelerisque auctor. Cras ultrices enim ut lectus pulvinar, ac semper
+                ex finibus. Cras feugiat faucibus molestie. Nunc pretium sit amet lacus eget pulvinar. Proin semper condimentum mi ac
+                gravida. Donec at vehicula lectus.
+                </p>
+                <p align="justify">
+                Aliquam aliquet enim et eros viverra, in mattis ante semper. Maecenas nec vestibulum nunc. Cras commodo nisl sed eros
+                volutpat dignissim. Suspendisse mollis mi ex, sit amet pretium augue sollicitudin nec. Curabitur vitae diam quis felis
+                pellentesque facilisis. Nulla quam massa, efficitur quis velit sed, finibus fringilla ante. Donec id mi blandit,
+                scelerisque sem ac, sagittis est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                curae;
+                </p>
+                <p align="justify">
+                Suspendisse hendrerit lorem volutpat, tempus libero quis, varius dolor. Cras fermentum, purus in maximus mollis, neque
+                massa placerat arcu, nec accumsan tortor urna eu massa. Nam velit ligula, tristique vitae blandit eu, vestibulum nec
+                erat. Vivamus id rhoncus velit, ac interdum sapien. Vivamus et bibendum nunc. Nam fringilla est sit amet ligula
+                tincidunt egestas. Suspendisse id diam ut risus suscipit lobortis. Integer efficitur massa sit amet sapien volutpat
+                vestibulum. Cras nulla diam, laoreet ac convallis ut, malesuada cursus ligula.
+                </p>`
+  ],
+  [
+    'https://images.unsplash.com/photo-1526346698789-22fd84314424?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
+    'chilli',
+    'Merawat Tanaman Cabai di Musim Penghujan',
+    'Aliquam aliquet enim et eros viverra, in mattis ante semper.',
+    `<p align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed justo est. Cras dignissim, turpis quis efficitur
+                faucibus, erat enim porta mi, ut mollis massa arcu sed mauris. In in ligula ac purus pretium rutrum venenatis sed enim.
+                Vivamus lacus mi, laoreet vel dignissim non, faucibus sed neque. Mauris venenatis malesuada velit. Etiam auctor, leo et
+                euismod luctus, neque tellus tempor dui, et lacinia ex ante sed tortor. Nunc elementum mollis lacus, sed facilisis
+                sapien malesuada ut. Quisque rhoncus nunc non diam scelerisque auctor. Cras ultrices enim ut lectus pulvinar, ac semper
+                ex finibus. Cras feugiat faucibus molestie. Nunc pretium sit amet lacus eget pulvinar. Proin semper condimentum mi ac
+                gravida. Donec at vehicula lectus.
+                </p>
+                <p align="justify">
+                Aliquam aliquet enim et eros viverra, in mattis ante semper. Maecenas nec vestibulum nunc. Cras commodo nisl sed eros
+                volutpat dignissim. Suspendisse mollis mi ex, sit amet pretium augue sollicitudin nec. Curabitur vitae diam quis felis
+                pellentesque facilisis. Nulla quam massa, efficitur quis velit sed, finibus fringilla ante. Donec id mi blandit,
+                scelerisque sem ac, sagittis est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                curae;
+                </p>
+                <p align="justify">
+                Suspendisse hendrerit lorem volutpat, tempus libero quis, varius dolor. Cras fermentum, purus in maximus mollis, neque
+                massa placerat arcu, nec accumsan tortor urna eu massa. Nam velit ligula, tristique vitae blandit eu, vestibulum nec
+                erat. Vivamus id rhoncus velit, ac interdum sapien. Vivamus et bibendum nunc. Nam fringilla est sit amet ligula
+                tincidunt egestas. Suspendisse id diam ut risus suscipit lobortis. Integer efficitur massa sit amet sapien volutpat
+                vestibulum. Cras nulla diam, laoreet ac convallis ut, malesuada cursus ligula.
+                </p>`
+  ],
+  [
+    'https://images.unsplash.com/photo-1510844355160-2fb07bf9af75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
+    'compos',
+    'Membuat Pupuk Kompos Dari Limbah Dapur',
+    'Aliquam aliquet enim et eros viverra, in mattis ante semper.',
+    `<p align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed justo est. Cras dignissim, turpis quis efficitur
+                faucibus, erat enim porta mi, ut mollis massa arcu sed mauris. In in ligula ac purus pretium rutrum venenatis sed enim.
+                Vivamus lacus mi, laoreet vel dignissim non, faucibus sed neque. Mauris venenatis malesuada velit. Etiam auctor, leo et
+                euismod luctus, neque tellus tempor dui, et lacinia ex ante sed tortor. Nunc elementum mollis lacus, sed facilisis
+                sapien malesuada ut. Quisque rhoncus nunc non diam scelerisque auctor. Cras ultrices enim ut lectus pulvinar, ac semper
+                ex finibus. Cras feugiat faucibus molestie. Nunc pretium sit amet lacus eget pulvinar. Proin semper condimentum mi ac
+                gravida. Donec at vehicula lectus.
+                </p>
+                <p align="justify">
+                Aliquam aliquet enim et eros viverra, in mattis ante semper. Maecenas nec vestibulum nunc. Cras commodo nisl sed eros
+                volutpat dignissim. Suspendisse mollis mi ex, sit amet pretium augue sollicitudin nec. Curabitur vitae diam quis felis
+                pellentesque facilisis. Nulla quam massa, efficitur quis velit sed, finibus fringilla ante. Donec id mi blandit,
+                scelerisque sem ac, sagittis est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                curae;
+                </p>
+                <p align="justify">
+                Suspendisse hendrerit lorem volutpat, tempus libero quis, varius dolor. Cras fermentum, purus in maximus mollis, neque
+                massa placerat arcu, nec accumsan tortor urna eu massa. Nam velit ligula, tristique vitae blandit eu, vestibulum nec
+                erat. Vivamus id rhoncus velit, ac interdum sapien. Vivamus et bibendum nunc. Nam fringilla est sit amet ligula
+                tincidunt egestas. Suspendisse id diam ut risus suscipit lobortis. Integer efficitur massa sit amet sapien volutpat
+                vestibulum. Cras nulla diam, laoreet ac convallis ut, malesuada cursus ligula.
+                </p>`
+  ],
+  [
+    'https://images.unsplash.com/photo-1528640936814-4460bc015292?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
+    'bugs',
+    'Daftar Penyakit Tanaman Yang Sering Menyerang Di Musim Hujan',
+    'Aliquam aliquet enim et eros viverra, in mattis ante semper.',
+    `<p align="justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed justo est. Cras dignissim, turpis quis efficitur
+                faucibus, erat enim porta mi, ut mollis massa arcu sed mauris. In in ligula ac purus pretium rutrum venenatis sed enim.
+                Vivamus lacus mi, laoreet vel dignissim non, faucibus sed neque. Mauris venenatis malesuada velit. Etiam auctor, leo et
+                euismod luctus, neque tellus tempor dui, et lacinia ex ante sed tortor. Nunc elementum mollis lacus, sed facilisis
+                sapien malesuada ut. Quisque rhoncus nunc non diam scelerisque auctor. Cras ultrices enim ut lectus pulvinar, ac semper
+                ex finibus. Cras feugiat faucibus molestie. Nunc pretium sit amet lacus eget pulvinar. Proin semper condimentum mi ac
+                gravida. Donec at vehicula lectus.
+                </p>
+                <p align="justify">
+                Aliquam aliquet enim et eros viverra, in mattis ante semper. Maecenas nec vestibulum nunc. Cras commodo nisl sed eros
+                volutpat dignissim. Suspendisse mollis mi ex, sit amet pretium augue sollicitudin nec. Curabitur vitae diam quis felis
+                pellentesque facilisis. Nulla quam massa, efficitur quis velit sed, finibus fringilla ante. Donec id mi blandit,
+                scelerisque sem ac, sagittis est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                curae;
+                </p>
+                <p align="justify">
+                Suspendisse hendrerit lorem volutpat, tempus libero quis, varius dolor. Cras fermentum, purus in maximus mollis, neque
+                massa placerat arcu, nec accumsan tortor urna eu massa. Nam velit ligula, tristique vitae blandit eu, vestibulum nec
+                erat. Vivamus id rhoncus velit, ac interdum sapien. Vivamus et bibendum nunc. Nam fringilla est sit amet ligula
+                tincidunt egestas. Suspendisse id diam ut risus suscipit lobortis. Integer efficitur massa sit amet sapien volutpat
+                vestibulum. Cras nulla diam, laoreet ac convallis ut, malesuada cursus ligula.
+                </p>`
+  ]
+];
+
+document.addEventListener('init', function (event) {
+  var page = event.target;
+
+  if (page.id === 'beranda') {
+    // tips page
+    page.querySelector('#tips').onclick = function () {
+        document.querySelector('#myNavigator').pushPage(
+            'pages/menu-tips.html',
+            {
+                data:
+                    { title: 'Page 2' }
+            });
+    };
+
+    // tool page
+    page.querySelector('#tool').onclick = function() {
+        document.querySelector('#myNavigator').pushPage(
+            'pages/menu-tool.html',
+            {
+                data:
+                    { title: 'Page 2' }
+            });
+    };
+
+    // seedings page
+    page.querySelector('#seedings').onclick = function() {
+        document.querySelector('#myNavigator').pushPage(
+            'pages/menu-seedings.html',
+            {
+                data:
+                    { title: 'Page 2' }
+            });
+    };
+
+    // ground page
+    page.querySelector('#ground').onclick = function() {
+        document.querySelector('#myNavigator').pushPage(
+            'pages/menu-ground.html',
+            {
+                data:
+                    { title: 'Page 2' }
+            });
+    };
+
+    // farmer page
+    page.querySelector('#farmer').onclick = function() {
+        document.querySelector('#myNavigator').pushPage(
+            'pages/menu-farmer.html',
+            {
+                data:
+                    { title: 'Page 2' }
+            });
+    };
+
+    // seed page
+    page.querySelector('#seed').onclick = function() {
+        document.querySelector('#myNavigator').pushPage(
+            'pages/menu-seed.html',
+            {
+                data:
+                    { title: 'Page 2' }
+            });
+    };
+  } else if(page.id === 'tips'){
+    var print = "";
+
+    for (var i = 0; i < list_tips.length; i++){
+      print += `
+        <div class="card">
+            <div class="card__content">
+                <h3 style="font-weight: bold;">${list_tips[i][2]}</h3>
+                <ul class="list">
+                    <li class="list-item">
+                        <div class="list-item__left">
+                            <img class="list-item__thumbnail" src="${list_tips[i][0]}" alt="${list_tips[i][1]}" style="width:80px !important;height:80px !important;">
+                        </div>
+                
+                        <div class="list-item__center">
+                            <div class="list-item__subtitle">
+                                ${list_tips[i][3]}
+                            </div>
+                            <div class="list-item__subtitle">
+                                <ons-button onclick="detail_tips(${i})" style="margin-top: 5px;">Detail Baca</ons-button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      `;
+
+      // print += 'print text ke-' + i + '<br />';
+    }
+
+    document.getElementById('tipList').innerHTML = print;
+  } else if (page.id === 'tips-detail') {
+
+    var img = myNavigator.topPage.data.img;
+    var alt = myNavigator.topPage.data.alt;
+    var title = myNavigator.topPage.data.title;
+    var text_detail = myNavigator.topPage.data.text_detail;
+
+    var print = "";
+
+    print = `<div>
+        <img src="${img}" alt="${alt}" width="100%">
+    </div>
+
+    <div style="height: 200px; padding: 1px 0 0 0;">
+        <div class="card">
+            <h2 class="card__title">${title}</h2>
+            <div class="card__content">
+              ${text_detail}
+            </div>
+        </div>
+    </div>`;
+  }
+
+  document.getElementById('loadTipsDetail').innerHTML = print;
+});
+
+function detail_tips($id) {
+  var img_url = list_tips[$id][0];
+  var img_alt = list_tips[$id][1];
+  var judul = list_tips[$id][2];
+  var detail = list_tips[$id][4]
+    document.querySelector('#myNavigator').pushPage(
+        'pages/detail-tips.html',
+        {
+            data:
+          {
+            img : img_url,
+            alt: img_alt,
+            title: judul,
+            text_detail : detail
+                }
+        });
+}
